3ebfdd54887ce8aaeb75c1e515c8c1641b5f69b5
tanishqkakkar <kakkartanishq13@gmail.com>
2024-01-11 19:29:06 +0530

    improve content

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,7 +79,7 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
+    console.log(selectedValue);
   for (let i = 0; i < filterItems.length; i++) {
 
     if (selectedValue === "all") {
